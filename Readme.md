4a2a1f99410d761e50083b6e61d05cbed24846af
Sandeep Akode <sandeep.akode@gmail.com>
2024-04-27 11:24:53 +0530

    react frontend using vite

--- Readme.md
+++ Readme.md
@@ -3,3 +3,10 @@
 ## Installation
 1. `docker build -t terno-ai .`
 2. `docker compose up`
+
+## How to develop frontend
+1. Make changes to src files in react frontend
+2. Run `npm run build` to generate assets
+3. Copy html templates to django frontend app
+4. Copy assets to frontend/static directory
+5. Run `python manage.py collectstatic --no-input`
--- terno/mysite/settings.py
+++ terno/mysite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sqlshield',
+    'frontend',
 ]
 
 MIDDLEWARE = [
@@ -116,8 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = 'static/'
+STATIC_URL = 'assets/'
+STATIC_ROOT = 'assets/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
