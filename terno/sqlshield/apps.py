59d2540b2b6481afd85f490ea4e95f2e1e2dedd8
Sandeep Akode <sandeep.akode@gmail.com>
2024-06-14 14:55:14 +0530

    format fix

--- terno/sqlshield/apps.py
+++ terno/sqlshield/apps.py
@@ -1,17 +1,9 @@
 from django.apps import AppConfig
-# from django.core.signals import setting_changed
-# from django.core.signals import post_save
 
-# def my_callback(sender, **kwargs):
-#     print("Request finished!")
 
 class SqlshieldConfig(AppConfig):
-    # default_auto_field = 'django.db.models.BigAutoField'
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'sqlshield'
-    # def ready(self):
-    #     # print("Ready! And settings up!")
-    #     # post_save.connect(my_callback)
-    #     # setting_changed.connect(my_callback)
-    #     # setting_changed.send(sender=self.__class__, toppings="toppings", setting=AppConfig, size="size")
-    #     # from .signals import data_source_changed
-    #     pass
+
+    def ready(self):
+        from . import receivers
