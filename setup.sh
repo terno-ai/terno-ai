95c19b051445c7d017eb2673e53094952a3f8f6c
Sandeep Akode <sandeep.akode@gmail.com>
2024-06-26 17:46:25 +0530

    removed superuser creation

--- setup.sh
+++ setup.sh
@@ -13,13 +13,13 @@ echo "Collecting static files..."
 python /code/terno/manage.py collectstatic --noinput
 
 # Check if superuser exists
-export DJANGO_SUPERUSER_PASSWORD=Superadmin@123
-echo -e "Your username: ${CYAN}admin${NC} password:" ${CYAN}$DJANGO_SUPERUSER_PASSWORD${NC}
+# export DJANGO_SUPERUSER_PASSWORD=Superadmin@123
+# echo -e "Your username: ${CYAN}admin${NC} password:" ${CYAN}$DJANGO_SUPERUSER_PASSWORD${NC}
 
-if python /code/terno/manage.py shell -c 'from django.contrib.auth.models import User; print(User.objects.filter(is_superuser=True).exists())' | grep -iq "false"; then
-    # Create superuser
-    echo "Creating superuser..."
-    python /code/terno/manage.py createsuperuser --username "admin" --email "admin@example.com" --noinput
-else
-    echo "Superuser already exists."
-fi
+# if python /code/terno/manage.py shell -c 'from django.contrib.auth.models import User; print(User.objects.filter(is_superuser=True).exists())' | grep -iq "false"; then
+#     # Create superuser
+#     echo "Creating superuser..."
+#     python /code/terno/manage.py createsuperuser --username "admin" --email "admin@example.com" --noinput
+# else
+#     echo "Superuser already exists."
+# fi
