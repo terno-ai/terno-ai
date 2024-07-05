9d8183ff57b0ca2d30277d19162e12038363bfdf
Sandeep Akode <sandeep.akode@gmail.com>
2024-07-05 12:29:14 +0530

    sidebar height fix

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -2,7 +2,7 @@ import SchemaPane from "./SchemaPane";
 
 const Sidebar = () => {
   return (
-    <div className="min-w-[300px] inline-flex flex-col bg-slate-100 py-[25px] px-[15px] overflow-y-auto">
+    <div className="min-w-[300px] h-screen inline-flex flex-col bg-slate-100 py-[25px] px-[15px] overflow-y-auto">
       <div className="w-full justify-center mt-[50px] flex items-center gap-[10px] py-[10px] px-[15px] bg-slate-300 rounded-[50px] text-md slate-500 cursor-pointer">
         <p>New Chat</p>
       </div>
