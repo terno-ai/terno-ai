e2ea041754e86371798f85697980f1ca2e16606b
Sandeep Akode <sandeep.akode@gmail.com>
2024-08-16 15:06:52 +0530

    duplicate column creation fixed

--- terno/terno/receivers.py
+++ terno/terno/receivers.py
@@ -13,26 +13,29 @@ def load_metadata(datasource):
             version_info = engine.dialect.server_version_info
             datasource.db_info = dialect_name + ' version ' + str(version_info)
             datasource.save(update_fields=['db_info'])
+
     inspector = sqlalchemy.inspect(engine)
     schemas = inspector.get_schema_names()
     current_tables = {}
     for schema in schemas[:1]:
         for table_name in inspector.get_table_names(schema=schema):
-            existing_tables = Table.objects.filter(name=table_name, data_source=datasource)
-            if len(existing_tables) > 0:
-                mtable = existing_tables[0]
+            existing_tables = Table.objects.filter(
+                name=table_name, data_source=datasource)
+            if existing_tables:
+                mtable = existing_tables.first()
             else:
-                mtable = Table(name=table_name, public_name=table_name, data_source=datasource)
-                mtable.save()
+                mtable = Table.objects.create(
+                    name=table_name, public_name=table_name,
+                    data_source=datasource)
             current_tabcols = []
             current_tables[table_name] = current_tabcols
             for col in inspector.get_columns(table_name, schema=schema):
-                dbcol = TableColumn.objects.filter(name=col, table=mtable)
-                if len(dbcol) > 0:
-                    tabcol = dbcol[0]
-                else:
-                    tabcol = TableColumn(name=col['name'], public_name=col['name'], table=mtable, data_type=col['type'])
-                    tabcol.save()
+                dbcol = TableColumn.objects.filter(
+                    name=col['name'], table=mtable)
+                if not dbcol:
+                    TableColumn.objects.create(
+                        name=col['name'], public_name=col['name'],
+                        table=mtable, data_type=col['type'])
                 current_tabcols.append(col)
     return current_tables
 
