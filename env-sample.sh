d37f191542df097da29d556fce8e9653d01bd6c9
Sandeep Akode <sandeep.akode@gmail.com>
2024-08-29 13:22:45 +0530

    fixed some env issues

--- env-sample.sh
+++ env-sample.sh
@@ -5,6 +5,6 @@ export MYSQL_DB=''
 export MYSQL_USER=''
 export MYSQL_PASS=''
 export CSRF_TRUSTED_ORIGINS=['http://127.0.0.1:8000']
-export SECRET_KEY=''
+export SECRET_KEY='xxxxx'
 export TERNO_LOG_FILE=''
-export TERNO_LOG_FILE:''
+export DEBUG='True'
--- terno/mysite/settings.py
+++ terno/mysite/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://app.terno.io', os.getenv('CSRF_TRUSTED_ORIGINS')]
