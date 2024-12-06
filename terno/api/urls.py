3e016464a9056083d84a8ff900f17dcd58307bac
sachingiri <sachin.g.eng@gmail.com>
2024-12-06 13:13:53 +0530

    added api for organisation creation

--- terno/api/urls.py
+++ terno/api/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'api'
 
 urlpatterns = [
-    path('organisation', views.get_org_details, name='get_org_details'),
-    path('user', views.get_user_details, name='get_user_details'),
+    path('/api/organisation', views.get_org_details, name='get_org_details'),
+    path('/api/user', views.get_user_details, name='get_user_details'),
 
 ]
