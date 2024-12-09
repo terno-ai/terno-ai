07416489436ec4686b3599e91dae13232db68e60
Sandeep Akode <sandeep.akode@gmail.com>
2024-12-09 15:30:45 +0530

    create user on org get

--- terno/api/views.py
+++ terno/api/views.py
@@ -9,7 +9,7 @@ import json
 def get_org_details(request):
     if request.method == "GET":
         user_email = request.GET.get('user')
-        user = authmodels.User.objects.get(email=user_email)
+        user = get_or_create_user(user_email)
         if user:
             user_organisations = OrganisationUser.objects.filter(
                 user=user).values_list('organisation', flat=True)
@@ -18,18 +18,18 @@ def get_org_details(request):
             for orgs in user_organisations:
                 org = orgs.organisation
                 organisation_details.append({
-                    'organisation_id': org.id,
-                    'organisaiton_name': org.name,
+                    # 'organisation_id': org.id,
+                    'organisation_name': org.name,
                     'organisation_subdomain': org.subdomain,
-                    'organisation_owner': org.owner,
-                    'organisation_logo': org.logo,
-                    'organisation_is_active': org.is_active,
+                    # 'organisation_owner': org.owner,
+                    # 'organisation_logo': org.logo,
+                    # 'organisation_is_active': org.is_active,
                 })
             return JsonResponse(
-                {"status": "success", "organisations": organisation_details}, status=200)
+                {"status": "success", "data": organisation_details}, status=200)
         else:
             return JsonResponse(
-                {"status": "error", "organisations": None}, status=200)
+                {"status": "error", "data": None}, status=200)
 
     if request.method == "POST":
         try:
@@ -37,7 +37,7 @@ def get_org_details(request):
         except json.JSONDecodeError:
             return JsonResponse({'status': 'error', 'message': 'Invalid JSON'}, status=400)
         user_email = data.get('user')
-        org_name = data.get('name')
+        org_name = data.get('org_name')
         subdomain = data.get('subdomain')
         try:
             user = get_or_create_user(user_email)
@@ -59,11 +59,12 @@ def get_user_details(request):
     user_email = request.GET.get('user')
     user = authmodels.User.objects.get(email=user_email)
     user_details = {
-        'id': user.id,
-        'username': user.username,
-        'email': user.email,
-        'first_name': user.first_name,
-        'last_name': user.last_name,
+        # 'id': user.id,
+        # 'username': user.username,
+        # 'email': user.email,
+        # 'first_name': user.first_name,
+        # 'last_name': user.last_name,
+        'full_name': user.get_full_name(),
     }
 
     return JsonResponse({
