d90c4f24552a8b95034c258a7c3950c1755a8499
sachingiri <sachin.g.eng@gmail.com>
2024-12-09 16:09:35 +0530

    fixed api urls

--- terno/api/urls.py
+++ terno/api/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'api'
 
 urlpatterns = [
-    path('/api/organisation', views.get_org_details, name='get_org_details'),
-    path('/api/user', views.get_user_details, name='get_user_details'),
+    path('organisation', views.get_org_details, name='get_org_details'),
+    path('user', views.get_user_details, name='get_user_details'),
 
 ]
--- terno/mysite/urls.py
+++ terno/mysite/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('terno.urls')),
     path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
 ]
