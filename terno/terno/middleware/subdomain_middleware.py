35ca8a8842c85ca4c78ed311bf48725471d34035
Sandeep Akode <sandeep.akode@gmail.com>
2024-12-10 19:25:01 +0530

    added subdomain middleware

---
+++ terno/terno/middleware/subdomain_middleware.py
@@ -0,0 +1,22 @@
+from django.http import HttpResponseForbidden
+from terno.models import Organisation
+
+
+class SubdomainOrganisationMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if '/api' in request.path or '/sso-login' in request.path:
+            return self.get_response(request)
+
+        host = request.get_host().split(':')[0]
+        subdomain = host.split('.')[0]
+        try:
+            organisation = Organisation.objects.get(subdomain=subdomain)
+            request.org_id = organisation.id
+        except Organisation.DoesNotExist:
+            return HttpResponseForbidden("Organisation doesn't exist")
+
+        response = self.get_response(request)
+        return response
