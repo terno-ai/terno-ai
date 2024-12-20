d2fd7bce8da09329274c944e7bfdfa14f23daee1
sachingiri <sachin.g.eng@gmail.com>
2024-12-20 13:09:18 +0530

    Restrict logins to their rightful subdomains

--- terno/terno/middleware/subdomain_middleware.py
+++ terno/terno/middleware/subdomain_middleware.py
@@ -1,6 +1,5 @@
 from django.http import HttpResponseForbidden
-from terno.models import Organisation
-import os
+from terno.models import Organisation, OrganisationUser
 
 
 class SubdomainOrganisationMiddleware:
@@ -8,16 +7,16 @@ class SubdomainOrganisationMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        if not os.getenv('ENABLE_SUBDOMAIN'):
-            return self.get_response(request)
-
-        if '/api' in request.path or '/sso-login' in request.path:
+        if '/api' in request.path or '/sso-login' in request.path or not request.user.is_authenticated:
             return self.get_response(request)
 
         host = request.get_host().split(':')[0]
         subdomain = host.split('.')[0]
         try:
             organisation = Organisation.objects.get(subdomain=subdomain)
+            if not OrganisationUser.objects.filter(
+                    user=request.user, organisation=organisation).exists():
+                return HttpResponseForbidden("You are not allowed to access this Organisation.")
             request.org_id = organisation.id
         except Organisation.DoesNotExist:
             return HttpResponseForbidden("Organisation doesn't exist")
