63bfed2d5988043d8b22fc32d5d2d6f8007d9924
Sandeep Akode <sandeep.akode@gmail.com>
2025-01-06 23:25:48 +0530

    Fixed the debug env bool issue

--- terno/mysite/settings.py
+++ terno/mysite/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.getenv('DEBUG'))
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://app.terno.io', os.getenv('CSRF_TRUSTED_ORIGINS')]
