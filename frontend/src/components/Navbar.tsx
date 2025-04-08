1140a8ee93c0cfc321b9eb8fccc4dac1d7481edc
anujd-cpu <anuj@cloudxlab.com>
2025-04-08 08:15:08 +0000

    Added mode toggle to Navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -19,7 +19,6 @@ import { ModeToggle } from "./chat/mode-toggle";
 
 const Navbar = () => {
   const { toggleSidebar } = useSidebar(); 
-  const { toggleSidebar } = useSidebar(); 
   const [user] = useUserDetails();
   const { ds } = useContext(DataSourceContext);
   const [response, setResponse] = useState({ fetching: false, content: null });
@@ -60,22 +59,9 @@ const Navbar = () => {
           <img src={terno} className="logo h-[40px]" alt="Terno logo" />
           <p className="font-semibold">Terno AI</p>
         </div>
-    <div className="flex items-center justify-between text-xl p-5 pl-2">
-      <div className="flex items-center space-x-3">  
-        <button
-          className="md:hidden p-2 text-gray-700 hover:text-black ml-[-8px]"
-          onClick={toggleSidebar}
-        >
-          <FaBars size={24} />
-        </button>
-        <div className="inline-flex items-center">
-          <img src={terno} className="logo h-[40px]" alt="Terno logo" />
-          <p className="font-semibold">Terno AI</p>
-        </div>
       </div>
+      <ModeToggle />
         <div className="font-semibold">{ds.name}</div>
-      
-        <ModeToggle />
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <button className="flex flex-row focus-visible:ring-0 font-normal text-base">
@@ -89,20 +75,6 @@ const Navbar = () => {
             <div className="font-light">{user.username}</div>
           </DropdownMenuLabel>
           <DropdownMenuSeparator />
-          <DropdownMenuItem
-            onSelect={(e) => {e.preventDefault()}}
-            className="hover:bg-slate-100"
-          >
-            <div className="w-full">
-              <button
-                onClick={() => window.open(("/admin"))}
-                className="flex w-full gap-2 items-center cursor-pointer"
-              >
-                <FaArrowUpRightFromSquare />
-                <span>Admin</span>
-              </button>
-            </div>
-          </DropdownMenuItem>
           <DropdownMenuItem
             onSelect={(e) => {e.preventDefault()}}
             className="hover:bg-slate-100"
