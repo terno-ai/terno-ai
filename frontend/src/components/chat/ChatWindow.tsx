e6774359ede70aacf076e091930e6127ee815e4d
Sandeep Giri <sandeepgiri@gmail.com>
2025-04-09 13:40:49 -0400

    Commenting the PromptBox

--- frontend/src/components/chat/ChatWindow.tsx
+++ frontend/src/components/chat/ChatWindow.tsx
@@ -1,10 +1,10 @@
-import PromptBox from "./PromptBox";
+// import PromptBox from "./PromptBox";
 
 const ChatWindow = () => {
   
   return (
     <div className="flex flex-1 justify-center items-center">
-      <PromptBox />
+      {/* <PromptBox /> */}
     </div>
   );
 };
