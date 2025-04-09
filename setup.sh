56d40207e9619d2868042057de7cfaf6ebe25395
sachingiri <sachin.g.eng@gmail.com>
2025-04-09 20:36:40 +0530

    Fix: Ensure proper excution of python script

--- setup.sh
+++ setup.sh
@@ -24,7 +24,7 @@ else
     echo "Superuser already exists."
 fi
 
-if python /code/terno-ai/terno/manage.py shell -c 'from django.contrib.auth.models import Group; print(Group.objects.filter(name='org_owner').exists())' | grep -iq "false"; then
+if python /code/terno-ai/terno/manage.py shell -c "from django.contrib.auth.models import Group; print(Group.objects.filter(name='org_owner').exists())" | grep -iq "false"; then
     #create a default group for organisation owner org_owner
     echo "Creating a default group for organisation owner org_owner"
     python /code/terno-ai/terno/manage.py shell -c 'from terno.utils import create_org_owner_group; create_org_owner_group()'
